32d1baf05e228df0d28c70a1dd5123f22dd9d6e9
Gaurav Mali <gauravmali846@gmail.com>
2024-01-14 21:24:46 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from './Component/header/Header'; 
 import FirstSection from './pages/FirstSection';
-import Hero from './pages/Hero';
+
 import SecondSection from './pages/SecondSection';
 import Loader from './Loader';
 
@@ -19,7 +19,7 @@ function App() {
           <Route path="/" element={<Loader />} />
    
           <Route path="/FirstSection" element={<FirstSection />} />
-          <Route path="/Hero" element={<Hero />} />
+       
 
           <Route path="/SecondSection" element={<SecondSection />} />
         </Routes>
