c099e2512271355f2846e285598f034a02504e36
Jaydeep Kulkarni <jay.kulkarni2005@gmail.com>
2024-01-15 12:09:12 +0530

    footer third section updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import Header from './Component/header/Header'; 
+import React, { useState, useEffect } from "react";
+import Header from './Component/header/Header';
+import Footer from './Component/footer/Footer';
 import FirstSection from './pages/FirstSection';
-import Preloader from './Preloader';
 import SecondSection from './pages/SecondSection';
+import ThirdSection from './pages/ThirdSection';
+import Preloader from './Preloader';
 
 function App() {
-  const location = useLocation(); 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000); // Adjust timer as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Preloader />;
+  }
 
   return (
     <>
-      {location.pathname !== '/' && <Header />} 
-      <Routes>
-        <Route path="/" element={<Preloader />} />
-        <Route path="/FirstSection" element={<FirstSection />} />
-        <Route path="/SecondSection" element={<SecondSection />} />
-      </Routes>
+      <Header />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <Footer />
     </>
   );
 }
